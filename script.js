a81e4d99173023a6919c55ed91089b612690b4e9
NIKHIL PAL <nikhilpal9312@gmail.com>
2025-12-11 12:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const getSumBtn = document.createElement("button");
+ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
-const getSum = () => {
-//Add your code here
-  
-};
-
-getSumBtn.addEventListener("click", getSum);
-
+getSumBtn.addEventListener("click", getsum = ()=>{
+let prices = document.querySelectorAll(".price")
+let sum = 0;
+prices.forEach(value => {
+    sum += Number(value.innerText);
+});
+let table = document.querySelector("table")
+let tablerow = document.createElement("tr")
+let tabledata = document.createElement("td")
+tabledata.colSpan = 2;
+tabledata.innerText = "table price = "+sum;
+tablerow.appendChild(tabledata)
+table.appendChild(tablerow)
+});
